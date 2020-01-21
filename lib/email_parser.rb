308e62f5acdd5eaaaa2e6f414f6b0208b5654774
S. Makai Takori <makait87@gmail.com>
2020-01-21 17:49:30 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,12 +7,10 @@ attr_accessor :emails
   end  
     
   def parse
-    # spacing is important when using .split method
+    # spacing is important when using .split method with delimiter
     
     splitEmails = @emails.split.collect {|address| address.split(",")}
-    
     newArray = splitEmails.flatten.uniq
     newArray
-  end  
-  
+  end
 end  
